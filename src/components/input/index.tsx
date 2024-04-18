ec17f4f8dac53760eb2f595a21dd19831282fce7
ebokyung <dlqhrud303@naver.com>
2024-04-18 22:24:38 +0900

    feat: label 여부 렌더링 변경

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -4,27 +4,16 @@ import { InputProps } from './input.types';
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ variant, size, type = 'text', placeholder = 'placeholder', label, className }, ref) => {
-    if (label) {
-      return (
-        <div>
-          <label>{label}</label>
-          <input
-            ref={ref}
-            type={type}
-            placeholder={placeholder}
-            className={`${inputStyles({ variant, size })} ${className ?? ''}`}
-          />
-        </div>
-      );
-    }
-
     return (
-      <input
-        ref={ref}
-        type={type}
-        placeholder={placeholder}
-        className={`${inputStyles({ variant, size })} ${className ?? ''}`}
-      />
+      <div>
+        {label ? <label>{label}</label> : null}
+        <input
+          ref={ref}
+          type={type}
+          placeholder={placeholder}
+          className={`${inputStyles({ variant, size })} ${className ?? ''}`}
+        />
+      </div>
     );
   },
 );
